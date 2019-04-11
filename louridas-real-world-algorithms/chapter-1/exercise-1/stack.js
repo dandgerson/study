19c40e3d98446f07c253f0a6e37739e463aafbf2
dandgerson <dandgerson@gmail.com>
2019-04-12 00:41:41 +0300

    refactor Stack

--- louridas-real-world-algorithms/chapter-1/exercise-1/stack.js
+++ louridas-real-world-algorithms/chapter-1/exercise-1/stack.js
@@ -1,38 +1,46 @@
-class Stack {
-  constructor(maxSize) {
-    if (maxSize) {
-      this.maxSize = maxSize;
-      this.data = new Array(maxSize);
-    } else this.data = [];
+const Stack = (function closure() {
+  const priv = new WeakMap();
+  const _ = instance => priv.get(instance);
+
+  class StackClass {
+    constructor(maxSize) {
+      const privatMembers = (maxSize && { data: new Array(maxSize) }) || { data: [] };
+      priv.set(this, privatMembers);
+
+      maxSize && (this.maxSize = maxSize);
+    }
+
+    push(i) {
+      if (this.maxSize && this.maxSize < _(this).data.length + 1) throw new Error('The Stack is full.');
+      _(this).data.push(i);
+    }
+
+    pop() {
+      if (this.isEmpty()) throw new Error('The Stack is empty.');
+      return _(this).data.pop();
+    }
+
+    top() {
+      if (this.isEmpty()) throw new Error('The Stack is empty.');
+      return _(this).data.slice(-1)[0];
+    }
+
+    getSize() {
+      return _(this).data.filter(i => i !== undefined).length;
+    }
+
+    isEmpty() {
+      return this.getSize() === 0;
+    }
+
+    getMaxSize() {
+      if (this.maxSize) return this.maxSize;
+      throw new Error('The Stack is dynamic.');
+    }
   }
 
-  push(i) {
-    if (this.maxSize && this.maxSize < this.data.length + 1) throw new Error('The Stack is full.');
-    this.data.unshift(i);
-  }
+  return StackClass;
+}());
 
-  pop() {
-    if (this.isEmpty()) throw new Error('The Stack is empty.');
-    return this.data.shift();
-  }
-
-  top() {
-    if (this.isEmpty()) throw new Error('The Stack is empty.');
-    return this.data[0];
-  }
-
-  getSize() {
-    return this.data.filter(i => i !== undefined).length;
-  }
-
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-
-  getMaxSize() {
-    if (this.maxSize) return this.maxSize;
-    throw new Error('The Stack is dynamic.');
-  }
-}
 
 module.exports = Stack;
